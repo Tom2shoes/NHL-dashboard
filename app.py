7e18a07041c78f4e95e44fbaa7ca513422a48f7b
Thomas Nakamoto <tnakamoto@me.com>
2018-12-06 19:52:37 -0800

    promise issues

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_pymongo import PyMongo
+import pandas as pd
+import json
 from stats_pull_store import *
 
 app = Flask(__name__)
@@ -8,10 +10,22 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def home_page():
-    team_stats = mongo.db.STATS.find_one()
     return render_template("index.html",
-        team_stats=team_stats,
         teams_array=teams_array)
+
+@app.route("/team_stats")
+def team_stats_data():
+        #team_stats = list(mongo.db.STATS.find({}, {'_id': False}))
+        return jsonify(list(stats_df.columns)[2:])
+
+@app.route("/stat.faceOffWinPercentage")
+def faceOffWinPercentage():
+        return jsonify(list(stats_df.loc[:, 'stat.faceOffWinPercentage']))
+
+        
+@app.route("/teamname")
+def teamName():
+        return jsonify(list(stats_df.loc[:, 'team.name']))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,89 +1,110 @@
-let teamList = ['New Jersey Devils', 'New York Islanders', 'New York Rangers', 'Philadelphia Flyers', 'Pittsburgh Penguins', 'Boston Bruins', 'Buffalo Sabres', 'Montréal Canadiens', 'Ottawa Senators', 'Toronto Maple Leafs', 'Carolina Hurricanes', 'Florida Panthers', 'Tampa Bay Lightning', 'Washington Capitals', 'Chicago Blackhawks', 'Detroit Red Wings', 'Nashville Predators', 'St. Louis Blues', 'Calgary Flames', 'Colorado Avalanche', 'Edmonton Oilers', 'Vancouver Canucks', 'Anaheim Ducks', 'Dallas Stars', 'Los Angeles Kings', 'San Jose Sharks', 'Columbus Blue Jackets', 'Minnesota Wild', 'Winnipeg Jets', 'Arizona Coyotes', 'Vegas Golden Knights']
-
-
-let gamesPlayed = [25, 25, 27, 25, 25, 26, 27, 26, 27, 27, 25, 25, 27, 26, 27, 26, 27, 25, 26, 26, 26, 29, 29, 27, 26, 27, 26, 26, 25, 25, 28]
-let wins = [9, 13, 13, 11, 10, 14, 17, 12, 12, 19, 12, 10, 19, 15, 9, 12, 18, 9, 15, 15, 13, 11, 14, 14, 9, 12, 15, 14, 15, 12, 14]
-let losses = [11, 9, 12, 12, 10, 8, 7, 9, 12, 8, 9, 10, 7, 8, 13, 11, 8, 13, 9, 6, 11, 15, 10, 10, 16, 10, 9, 10, 8, 11, 13]
-let goalsPerGame = [2.96, 3.0, 2.704, 3.0, 3.32, 2.692, 3.037, 3.077, 3.63, 3.63, 2.6, 3.24, 3.778, 3.56, 2.778, 2.846, 3.259, 2.96, 3.385, 3.731, 2.692, 2.793, 2.214, 2.741, 2.115, 3.037, 3.462, 3.192, 3.48, 2.6, 2.893]
-
-
-let scrollTable = d3.select('.list-group')
-
-// itterates though teamList and appends a new selection
-// teamList.forEach(function(d) {
-//   console.log(d);
-//   scrollTable.append("li")
-//     .attr('class', "list-group-item list-group-item-action")
-//     .text(d)
-// })
-
-
-
-
-let trace1 = {
-  x: gamesPlayed,
-  y: teamList.slice(0, 6),
-  xaxis: 'x',
-  yaxis: 'y',
-  name: 'Games Played',
-  type: 'bar',
-  orientation: 'h'
-};
-
-let trace2 = {
-  x: losses,
-  y: teamList.slice(0, 6),
-  xaxis: 'x2',
-  yaxis: 'y2',
-  name: 'Losses',
-  type: 'bar',
-  orientation: 'h'
-};
-
-let trace3 = {
-  x: wins,
-  y: teamList.slice(0, 6),
-  xaxis: 'x3',
-  yaxis: 'y3',
-  name: 'Wins',
-  type: 'bar',
-  orientation: 'h'
-};
-
-let trace4 = {
-  x: goalsPerGame,
-  y: teamList.slice(0, 6),
-  xaxis: 'x4',
-  yaxis: 'y4',
-  name: 'Goals Per Game',
-  type: 'bar',
-  orientation: 'h'
-};
-
-
-let data = [trace1, trace2, trace3, trace4];
-
-let layout = {
-  grid: {
-    rows: 2, 
-    columns: 2, 
-    pattern: 'independent'
-  },
-  showlegend: true,
-  legend: {"orientation": "h"},
-  yaxis: {
-    tickfont: {
-      size: 5
-    }
-  },
-  margin: {
-    l: 150,
-    r: 50,
-    b: 50,
-    t: 50,
-    pad: 10
-  },
+let selectedTeams = []
+choiceTable = d3.selectAll('.list-group-item.list-group-item-action');
   
-};
+choiceTable
+  .on('click', function() {
+    d3.select(this)
+      .style('color', 'red');
+      selectedTeams.push(this.innerHTML);
+      console.log(selectedTeams);
+  });
 
-Plotly.newPlot('myDiv', data, layout);
+
+let gamesPlayed = []
+let wins = []
+let losses = []
+let goalsPerGame = []
+
+function random_rgba() {
+  let o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+let faceOffWin = []
+let promise2 = d3.json("/stat.faceOffWinPercentage", function(data) {
+  data.forEach(d => {
+    faceOffWin.push(d);
+  })
+  console.log(faceOffWin);
+});
+
+let teamList = []
+let promise1 = d3.json("/teamname", function(data) {
+  data.forEach(d => {
+    teamList.push(d);
+    console.log(typeof(d))
+  })
+  console.log(teamList[0]);
+});
+
+// let radarDataset = []
+// Promise.all([promise1, promise2]).then(
+//   for (i = 0; i < teamList.length; i++) {
+//   radarDataset.push(
+//     {
+//     "label": teamList[i],
+//     "fill": true,
+//     "backgroundColor": random_rgba(i),
+//     "borderColor": random_rgba(i),
+//     "pointBorderColor": random_rgba(i),
+//     "pointBackgroundColor": random_rgba(i),
+//     "data": faceOffWin[i]
+//   }}
+// ));
+
+// console.log(radarDataset)
+
+let radarDataset = []
+Promise.all([promise1, promise2]).then(function(d) {
+  console.log(teamList)}
+  // for (i = 0; i < teamList.length; i++) {
+  //   console.log(radarDataset)
+  //   radarDataset.push(
+  //     {
+  //       label: teamList[i],
+  //       fill: true,
+  //       backgroundColor: random_rgba(i),
+  //       borderColor: random_rgba(i),
+  //       pointBorderColor: random_rgba(i),
+  //       pointBackgroundColor: random_rgba(i),
+  //       data: faceOffWin[i]
+  //     }
+  );
+  
+
+  
+
+
+
+
+
+
+// d3.json("/team_stats", function(data) {
+//   for (i = 0; i < 31; i++) {
+//   teamList.push(data[i]["stats"][0]["splits"][0]["team"]["name"]);
+//   gamesPlayed.push(data[i]["stats"][0]["splits"][0]["stat"]["gamesPlayed"]);
+//   wins.push(data[i]["stats"][0]["splits"][0]["stat"]["wins"]);
+//   losses.push(data[i]["stats"][0]["splits"][0]["stat"]["losses"]);
+//   goalsPerGame.push(data[i]["stats"][0]["splits"][0]["stat"]["goalsPerGame"]);
+//   console.log(teamList[i])
+// }});
+// new Chart(document.getElementById("radar-chart"), {
+
+//   type: 'radar',
+//   data: {
+//     labels: [
+//       "Face Off Win Percentage", 
+//       "Goals Against Per Game", 
+//       "Goals Per Game", 
+//       "Save Percentage", 
+//       "Shooting"
+//     ],
+//     datasets: radarDataset
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: 'NHL Team Statistics'
+//     }
+//   }
+// };
