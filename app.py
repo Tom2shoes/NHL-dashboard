28f50c2d2c4e29bdff4156c651d27de18ca690ab
sherryjystad <38591924+sherryjystad@users.noreply.github.com>
2018-12-10 12:20:36 -0800

    reading from TWITTER not TWITTER_Sentiment
    want to use x-axis with 100 for each tweet and when tweets were posted.

--- app.py
+++ app.py
@@ -1,34 +1,60 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-from flask import Flask, render_template, redirect
+# import necessary libraries
+import pandas as pd
 from flask_pymongo import PyMongo
-import NHL_Twitter_Sentiments
 
+from flask import Flask,render_template, jsonify,redirect
+from pymongo import MongoClient
 
+#import scrape_twitter
+
+# create instance of Flask app
 app = Flask(__name__)
 
+# Create connection 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/nhl-database"
 mongo = PyMongo(app)
 
+# Set route
 @app.route('/')
-def index():
-    TWITTER = mongo.db.TWITTER.find_one()
-    return render_template('index.html', TWITTER=TWITTER)
+def landing_page():
+    return render_template("index.html")
 
-@app.route('/scrape')
-def scrape():
-    TWITTER = mongo.db.TWITTER
-    TWITTER_data_scrape = NHL_Twitter_Sentiments.scrape()
-    TWITTER.update(
-        {},
-        TWITTER_data_scrape,
-        upsert=True
-    )
-    return redirect("Twitter Scraping Successful!!")
+#@app.route("/scrape_twitter")
+#def scrape_twitter():
+#    twitter_sentiment = mongo.db.TWITTER_SENTIMENT
+#    twitter_data_scrape = scrape_twitter.scrape()
+#    twitter_sentiment.update(
+#        {}, 
+#        twitter_data_scrape, 
+#        upsert=True
+#        )
+#   return redirect("Scraping Successful!!")
 
+@app.route("/twitter/all")
+def twitter():
+    twitter_data = []
+
+#    for sentiment in data:
+#       twitter_data.append({
+#            "Compound": sentiment["average_compound_score"],
+#            "Positive": sentiment["average_positive_score"],
+#            "Neutral": sentiment["average_neutral_score"],
+#            "Negative": sentiment["average_negative_score"]
+#        })
+
+    data = mongo.db.TWITTER.find({}, {'_id': False})
+    for sentiment in data:
+        twitter_data.append({
+            "hockey_team": sentiment["hockey_team"],
+            "team_counter": sentiment["team_counter"],
+            "neutral": sentiment["neutral"],
+            "positive": sentiment["positive"],
+            "created_at": sentiment["created_at"],
+            "negative": sentiment["negative"],
+            "text": sentiment["text"],
+            "compound": sentiment["compound"]
+        })
+    return jsonify(twitter_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
